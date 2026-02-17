e0f5cfe63d6adc738190e6d80acc351bac2458db
germanmalykh <germanmalyh@gmail.com>
2026-02-17 12:49:31 +0300

    JDBC (vol 3)

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/ui/tests/FriendsTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/ui/tests/FriendsTest.java
@@ -22,40 +22,49 @@ public class FriendsTest extends BaseUiTest {
     @Test
     @DisplayName("Display confirmed friends")
     void shouldDisplayConfirmedFriends(@User(WITH_FRIENDS) UserJson user) {
-        step("Login as " + user.username(), () -> main.open().clickLoginButton()
-                .getPage(AuthPage.class)
-                .loginAs(user.username(), user.testData().password())
-                .clickLoginButton());
+        step("Login as " + user.username(), () -> {
+            main.open().clickLoginButton()
+                    .getPage(AuthPage.class)
+                    .loginAs(user.username(), user.testData().password())
+                    .clickLoginButton();
+        });
         FriendsPage friendsPage = main.getPage(FriendsPage.class);
         step("Open Friends page", friendsPage::open);
-        step("Verify friendship is confirmed", () ->
-                assertTrue(friendsPage.hasConfirmedFriendship(), "Friendship should be confirmed"));
+        step("Verify friendship is confirmed", () -> {
+            assertTrue(friendsPage.hasConfirmedFriendship(), "Friendship should be confirmed");
+        });
     }
 
     @Test
     @DisplayName("Display incoming invitation")
     void shouldDisplayIncomingInvitation(@User(INCOMING_INVITE) UserJson user) {
-        step("Login as " + user.username(), () -> main.open().clickLoginButton()
-                .getPage(AuthPage.class)
-                .loginAs(user.username(), user.testData().password())
-                .clickLoginButton());
+        step("Login as " + user.username(), () -> {
+            main.open().clickLoginButton()
+                    .getPage(AuthPage.class)
+                    .loginAs(user.username(), user.testData().password())
+                    .clickLoginButton();
+        });
         FriendsPage friendsPage = main.getPage(FriendsPage.class);
         step("Open Friends page", friendsPage::open);
-        step("Verify incoming invitation is displayed", () ->
-                assertTrue(friendsPage.hasIncomingInvitation(), "Incoming invitation should be displayed"));
+        step("Verify incoming invitation is displayed", () -> {
+            assertTrue(friendsPage.hasIncomingInvitation(), "Incoming invitation should be displayed");
+        });
     }
 
     @Test
     @DisplayName("Display pending invitation")
     void shouldDisplayPendingInvitation(@User(PENDING) UserJson user) {
-        step("Login as " + user.username(), () -> main.open().clickLoginButton()
-                .getPage(AuthPage.class)
-                .loginAs(user.username(), user.testData().password())
-                .clickLoginButton());
+        step("Login as " + user.username(), () -> {
+            main.open().clickLoginButton()
+                    .getPage(AuthPage.class)
+                    .loginAs(user.username(), user.testData().password())
+                    .clickLoginButton();
+        });
         PeoplePage peoplePage = main.getPage(PeoplePage.class);
         step("Open People page", peoplePage::open);
-        step("Verify pending invitation is displayed", () ->
-                assertTrue(peoplePage.hasPendingInvitation(), "Pending invitation should be displayed"));
+        step("Verify pending invitation is displayed", () -> {
+            assertTrue(peoplePage.hasPendingInvitation(), "Pending invitation should be displayed");
+        });
     }
 
 }
